ec2671babe062205900b8be6dc619fd3b9a5bfb9
KimRanA <triple3dimple@gmail.com>
2019-06-24 11:22:53 +0900

    CREATE base.html & structure

--- hamlet/views.py
+++ hamlet/views.py
@@ -3,3 +3,40 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def A_name_list(request):
+    return render(request, 'A_name_list.html')
+
+def A1_easy_name(request):
+    return render(request, 'A1_easy_name.html')
+
+def A2_global_name(request):
+    return render(request, 'A2_global_name.html')
+
+def A3_pure_name(request):
+    return render(request, 'A3_pure_name.html')
+
+def A4_popular_name(request):
+    return render(request, 'A4_popular_name.html')
+
+def A5_fetus_name(request):
+    return render(request, 'A5_fetus_name.html')
+
+def B_nick_name_list(request):
+    return render(request, 'B_nick_name_list.html')
+
+def B1_random_nick_name(request):
+    return render(request, 'B1_random_nick_name.html')
+
+def B2_novel_nick_name(request):
+    return render(request, 'B2_novel_nick_name.html')
+
+def C_others_list(request):
+    return render(request, 'C_others_list.html')
+
+def C1_random(request):
+    return render(request, 'C1_random.html')
+
+def C2_lotto(request):
+    return render(request, 'C2_lotto.html')
+
