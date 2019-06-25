fdc1557280fe4e94a2302182b14b795cda81857b
KimRanA <triple3dimple@gmail.com>
2019-06-25 17:46:25 +0900

    Add index page

--- mooramoora/urls.py
+++ mooramoora/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from hamlet import views
 
 urlpatterns = [
+    path('', views.index),
     path('hamlet/', include('hamlet.urls')),
     path('admin/', admin.site.urls),
 ]
